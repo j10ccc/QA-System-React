74bd3bf83fe048593c42e6cf330fab2668b1b4ef
j10c <blyb1739@gmail.com>
2022-05-11 23:19:31 +0800

    🐛 fix: 题目过长无法换行显示

--- src/components/EachQuection.tsx
+++ src/components/EachQuection.tsx
@@ -5,18 +5,24 @@ enum TopicEnum {
   单选 = 1,
   多选,
 }
+function SuffixTag(props: any) {
+  const style = {
+    color: 'gray',
+  }
+  return (
+    <span style={style}>
+      {`(${TopicEnum[props.type]}) `}
+    </span>
+  )
+}
 export default function EachQuestion(props: any) {
   const { item, index, toggleAns, field } = props;
   return (
     <>
       <List header={'第 ' + `${index + 1}` + ' 题'} className='question'>
         <ListItem key={index}>
-          <Space>
-            <span>{item.topic}</span>
-            <Tag color={item.type === 2 ? 'primary' : undefined}>
-              {TopicEnum[item.type]}
-            </Tag>
-          </Space>
+          <SuffixTag type={item.type} ></SuffixTag>
+          <span style={{ wordBreak: 'break-all' }}>{item.topic}</span>
         </ListItem>
         <CheckList
           multiple={item.type == 2}
